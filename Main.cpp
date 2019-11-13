642e314bb36dd48bd306b45fc857794299107774
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-11-13 11:45:58 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,8 @@
 #include "VectorArray.h"
 #include "FactorArray.h"
 #include "MatrixArray.h"
-
+#include "list.h"
+#include "PriorityQueue.h"
 
 template <typename T>
 void print_array(IArray<T>* array) {
@@ -30,7 +31,7 @@ void test_remove_ind(IArray<T>* array) {
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
 	size_t size = array->size();
-	for (int i = size-1; i > 0; i--) {
+	for (int i = size-1; i >= 0; i--) {
 		array->remove(i);
 	}
 	end = std::chrono::system_clock::now();
@@ -113,80 +114,80 @@ void test_add_rem_array(IArray<T>* array) {
 
 int main() {
 	
+	//создаем объекты структыр данных
 	IArray<int>* sa = new SingleArray<int>();
 	IArray<int>* va = new VectorArray<int>(200);
 	IArray<int>* fa = new FactorArray<int>(2);
 	IArray<int>* ma = new MatrixArray<int>(100);
-	//sa->add(5);
-	//std::cout << (sa->size()) << std::endl;
-	//std::cout << (sa->get(1)) << std::endl;
-	test_add_Array(sa, 1205500);
+	
+	//заполнение данными
+	test_add_Array(sa, 205500); //120500 долго.
 	test_add_Array(va, 1205500);
 	test_add_Array(fa, 1205500);
 	test_add_Array(ma, 1205500);
-
+	
+	//доступ к кажому элементу по индексу
 	test_get_ind(sa);
 	test_get_ind(va);
 	test_get_ind(fa);
 	test_get_ind(ma);
-
+	
+	//добавляем первый элемент
 	test_add_first(sa);
 	test_add_first(va);
 	test_add_first(fa);
 	test_add_first(ma);
 
+	//добавляем элемент в середину
 	test_add_ind(sa);
 	test_add_ind(va);
 	test_add_ind(fa);
 	test_add_ind(ma);
 
+	//удаляем первый элемент	
 	test_rem_first(sa);
 	test_rem_first(va);
 	test_rem_first(fa);
 	test_rem_first(ma);
 
+	//удаляем элемент по индексу из середины
 	test_rem_ind(sa);
 	test_rem_ind(va);
 	test_rem_ind(fa);
 	test_rem_ind(ma);
 	
+	//удаляем все элементы начиная с последнего элемента
 	test_remove_ind(sa);
 	test_remove_ind(va);
 	test_remove_ind(fa);
 	test_remove_ind(ma);
 
+	//проверка размера
+	std::cout << sa->size() << std::endl;
+	std::cout << va->size() << std::endl;
+	std::cout << fa->size() << std::endl;
+	std::cout << ma->size() << std::endl;
 	
+	//тест для очереди с приоритетом. Добавляем 6 элементов с разным приоритетом. 
+	//извлекаем и выводим на экран элементы из очереди в порядке приоритета, убеждаемся, что размер очереди стал 0 после извлечения всех элементов.
+	PriorityQueue<int> q;
+	q.enqueue(3, 1);
+	q.enqueue(3, 2);
+	q.enqueue(1, 3);
+	q.enqueue(1, 4);
+	q.enqueue(2, 5);
+	q.enqueue(0, 6);
 
+	std::cout << q.dequeue() << std::endl;
+	std::cout << q.dequeue() << std::endl;
+	std::cout << q.dequeue() << std::endl;
+	std::cout << q.dequeue() << std::endl;
+	std::cout << q.dequeue() << std::endl;
+	std::cout << q.dequeue() << std::endl;
+	std::cout << q.size() << std::endl;
 
-	//test_add_rem_array(fa);
-
-	/*
-	std::cout << (sa->size()) << std::endl;
-	sa->add(1);
-	std::cout << (sa->size()) << std::endl;
-	std::cout << (sa->get(1)) << std::endl;
-	print_array(sa);
-	sa->add(2);
-	std::cout << (sa->size()) << std::endl;
-	std::cout << (sa->get(1)) << std::endl;
-	std::cout << (sa->get(2)) << std::endl;
-	print_array(sa);
-	sa->add(3);
-	std::cout << (sa->size()) << std::endl;
-	std::cout << (sa->get(1)) << std::endl;
-	std::cout << (sa->get(2)) << std::endl;
-	std::cout << (sa->get(3)) << std::endl;
-	std::cout << (sa->size()) << std::endl;
-	std::cout << (sa->get(5)) << std::endl;
-	print_array(sa);
-	std::cout << (sa->remove(5)) << std::endl;
-	print_array(sa);
-	std::cout << (sa->get(5)) << std::endl;
-	std::cout << (sa->size()) << std::endl;
-	print_array(sa);
 	
 	
-	*/
 
 	return 0;
 }
